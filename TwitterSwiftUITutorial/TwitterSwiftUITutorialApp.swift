0b8b42f77a3788e3c404e067541c35498b2bcb90
Daryl <daryl.9@gmail.com>
2021-07-26 20:04:15 +0800

    Initial Commit

---
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUITutorialApp.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by admin on 26/7/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
